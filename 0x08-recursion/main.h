9bff1cb76de808d1a6dce864bf6929fd959622d7
Ellajessica <emmanuellachinenye767@gmail.com>
2022-07-20 16:33:14 -0700

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,15 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
 /**
- * File: holberton.h
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x07-recursion directory.
- */
-
-int _putchar(char c);
+*void prototypes(void)
+*int prototypes(int)
+*char prototypes(char) 
+*/
 void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
+ void _print_rev_recursion(char *s);
+int _putchar(char c);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
@@ -17,5 +17,9 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int _sqrt(int n, int x);
+int is_prime(int n, int start);
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
 
-#endif
+#endif /* MAIN_H */
